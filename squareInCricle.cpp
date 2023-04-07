2700aaead9bd2c2ff4c8889b61a2190e58cb8d05
김익현/연구원/Audio Unit(ikhyeon.kim@lge.com) <ikhyeon.kim@lge.com>
2023-04-07 15:51:10 +0900

    init SIC

---
+++ squareInCricle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int R;//원의 반지름
+
+void InputData(){
+	cin >> R;
+}
+
+int main() {
+	int ans;
+	InputData();//입력
+	
+	//여기서부터 작성
+	
+	cout << ans << endl;//출력
+	return 0;
+}
