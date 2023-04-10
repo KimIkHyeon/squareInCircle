24dd1923121eb1cce773613363ee9a778726a2c6
김익현/연구원/Audio Unit(ikhyeon.kim@lge.com) <ikhyeon.kim@lge.com>
2023-04-10 09:46:43 +0900

    Complete Square in Circle

--- squareInCricle.cpp
+++ squareInCricle.cpp
@@ -12,6 +12,13 @@ int main() {
 	InputData();//입력
 	
 	//여기서부터 작성
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1;  j <= R; j++) {
+			if (i*i+j*j <= R*R) ans++;
+		}
+	}	
+	
+	ans *= 4;
 	
 	cout << ans << endl;//출력
 	return 0;
